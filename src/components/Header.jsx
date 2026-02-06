64f4b0eef12f8f7320b458ee137d0423017f86a2
sameertiruwa1010 <sameertiruwa1010@gmail.com>
2026-02-06 20:14:48 +0545

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
                 <h1 className="logo">DevOps Portfolio</h1>
                 <ul className="nav-links">
                     <li><a href="#home">Home</a></li>
-                    <li><a href="#skills">Skills</a></li>
+                    <li><a href="#skills">Skillssss</a></li>
                     <li><a href="#projects">Projects</a></li>
                     <li><a href="#contact">Contact</a></li>
                 </ul>
